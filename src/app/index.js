796c3c7029a68f9f7121b4b5395e019f3a03e6e3
Dmitiy <shkarlat.dmitry@yandex.ru>
2019-02-22 21:23:54 +0300

    add inputs

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,31 @@
 (() => {
 	console.log('ok');
+
+	const inputs = document.querySelectorAll('.input');
+
+	inputs.forEach(function (item) {
+		item.addEventListener('focus', onFocus, true);
+		item.addEventListener('blur', onBlur, true);
+	})
+
+
+	function onFocus(event) {
+		const el = event.target;
+		const input_box = el.parentNode;
+
+		input_box.classList.remove('has-error')
+	}
+
+	function onBlur(event) {
+		const el = event.target;
+		const input_box = el.parentNode;
+
+		if (el.value === '') {
+			input_box.classList.add('has-error')
+			input_box.classList.remove('has-value')
+		} else {
+			input_box.classList.remove('has-error')
+			input_box.classList.add('has-value')
+		}
+	}
 })();
