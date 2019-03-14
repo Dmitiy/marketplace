8def2e641b0fb3f24a9a9dfe39f9ee838395378d
Dmitiy <shkarlat.dmitry@yandex.ru>
2019-03-14 10:14:18 +0300

    fix: product js

--- src/app/assets/js/product.js
+++ src/app/assets/js/product.js
@@ -2,58 +2,62 @@ import {
     store
 } from './store';
 
-const navHeader = document.querySelectorAll('header .navigation a');
+const navigation = document.querySelectorAll('.nav a');
 
-navHeader.forEach(function (el) {
-
+navigation.forEach(function (el) {
     const link = el;
-    let html;
 
-    html = store.chairs.map((product) => renderItem(product)).join('');
+    let html = store.chairs.map(product => renderItem(product)).join('');
     document.querySelector('.products-list').innerHTML = html;
 
     link.addEventListener('click', function (e) {
         e.preventDefault();
         const active = e.target;
-        const product = e.target.textContent;
+        const product = e.target.dataset.name;
+        console.log(product);
+
+        let a;
 
         switch (product) {
-            case 'Стулья и кресла':
-                html = store.chairs.map((product) => renderItem(product)).join('');
-                document.querySelector('.products-list').innerHTML = html;
+            case 'chair':
+                a = 'chairs';
                 break;
-            case 'Кровати и матрасы':
-                html = store.beds.map((product) => renderItem(product)).join('');
-                document.querySelector('.products-list').innerHTML = html;
+            case 'bed':
+                a = 'beds';
                 break;
-            case 'Шкафы и комоды':
-                html = store.cupboard.map((product) => renderItem(product)).join('');
-                document.querySelector('.products-list').innerHTML = html;
+            case 'cupboard':
+                a = 'cupboard';
                 break;
-            case 'Мебель для кухни':
-                html = store.kitchen.map((product) => renderItem(product)).join('');
-                document.querySelector('.products-list').innerHTML = html;
+            case 'kitchen':
+                a = 'kitchen';
                 break;
-            case 'Товары для дома':
-                html = store.household.map((product) => renderItem(product)).join('');
-                document.querySelector('.products-list').innerHTML = html;
+            case 'household':
+                a = 'household';
+                break;
+            default:
                 break;
         }
-    });
-})
 
+        html = store[a].map(product => renderItem(product)).join('');
+        document.querySelector('.products-list').innerHTML = html;
+    });
+});
 
 function renderItem(item) {
     return `
-        <a class="${item.hasClass || 'product-link' }" href=${item.href}>
+        <a class="${item.hasClass || 'product-link'}" href=${item.href}>
             <figure class="product-item ">
                 <div class="product-item__img">
                     <img src=${item.imgSrc} alt=${item.title}>
                 </div>
                 <figcaption class="product-item__desc">
-                    <p class="product-item__title overflow-ellipsis">${item.title}</p>
+                    <p class="product-item__title overflow-ellipsis">${
+						item.title
+					}</p>
                     <p class="product-item__price">${item.price}
-                        <span class="product-item__currency">${item.currency}</span>
+                        <span class="product-item__currency">${
+							item.currency
+						}</span>
                     </p>
                     <button class="btn">${item.button || 'В корзину'}</button>
                 </figcaption>
