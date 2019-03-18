a4d70c611a951b8addbc101c4231c83013846818
Dmitiy <shkarlat.dmitry@yandex.ru>
2019-03-18 13:45:13 +0300

    add: routing

--- src/angular/src/app/app-routing.module.ts
+++ src/angular/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CatalogPageComponent } from './pages/catalog/catalog-page.component.ts';
+import { CartPageComponent } from './pages/cart/cart-page.component.ts';
+import { DetailItemPageComponent } from './pages/detail-item/detail-item-page.component.ts';
+import { DetailOrderPageComponent } from './pages/detail-order/detail-order-page.component.ts';
+import { OrderStatusPageComponent } from './pages/order-status/order-status-page.component.ts';
+
 const routes: Routes = [
-  // {path: '', component: ''}
+  { path: '', redirectTo: '/catalog', pathMatch: 'full' },
+  { path: 'catalog', component: 'CatalogPageComponent' },
+  {
+    path: 'catalog/:id',
+    component: 'DetailItemPageComponent',
+  },
+  { path: 'cart', component: 'CartPageComponent' },
+  { path: 'detail-item', component: 'DetailItemPageComponent' },
+  { path: 'detail-order', component: 'DetailOrderPageComponent' },
+  { path: 'order-status', component: 'OrderStatusPageComponent' },
 ];
 
 @NgModule({
