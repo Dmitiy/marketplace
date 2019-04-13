c95d4864f88fe597e5a9bbe0befc26d8685fac9c
Dmitiy <shkarlat.dmitry@yandex.ru>
2019-04-13 11:37:45 +0300

    add: repair detail-order routing

--- src/angular/src/app/app-routing.module.ts
+++ src/angular/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: ':category/:id', loadChildren: './pages/product-detail-info-page/product-detail-info-page.module#ProductDetailInfoPageModule' },
   { path: 'cart', loadChildren: './pages/cart-page/cart-page.module#CartPageModule' },
   { path: 'check-status', loadChildren: './pages/check-status-page/check-status-page.module#CheckStatusPageModule' },
-  { path: 'detail-order', loadChildren: './pages/detail-order-page/detail-order-page.component#DetailOrderPageModule' },
+  { path: 'detail-order', loadChildren: './pages/detail-order-page/detail-order-page.module#DetailOrderPageModule' },
   { path: '**', loadChildren: './pages/page-not-found/page-not-found.module#PageNotFoundModule' },
 ];
 
